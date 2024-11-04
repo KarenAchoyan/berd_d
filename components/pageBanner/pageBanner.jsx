4497bcad6351902529dae3d537bca29e84cdeb41
Karen <karenachoyan8@gmail.com>
2024-11-04 20:32:10 +0400

    Change image

--- components/pageBanner/pageBanner.jsx
+++ components/pageBanner/pageBanner.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import styles from "../../styles/pageBanner.module.css"
+import Image from "next/image";
 const PageBanner = ({title}) => {
     return (
         <div className={styles.banner}>
+            <Image width={1000} height={600} src="/performance1.png" alt={"Banner"}/>
             <div className={styles.overlay}>
                 <h2>{title}</h2>
             </div>
